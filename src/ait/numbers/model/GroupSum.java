28f9ed9641d3639a558155c2388aa1e7cf39efc0
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-13 18:11:39 +0200

    ThreadGroupSum done

--- src/ait/numbers/model/GroupSum.java
+++ src/ait/numbers/model/GroupSum.java
@@ -1,11 +1,31 @@
 package ait.numbers.model;
 
-public abstract class GroupSum {
+import java.util.Arrays;
+
+public abstract class GroupSum implements Runnable {
     protected int[][] numberGroups;
+    protected int rowIndex;
+    protected int rowSum;
 
     public GroupSum(int[][] numberGroups) {
         this.numberGroups = numberGroups;
     }
 
+    // Constructor for task creation
+    public GroupSum(int[][] numberGroups, int rowIndex) {
+        this.numberGroups = numberGroups;
+        this.rowIndex = rowIndex;
+    }
+
     public abstract int computeSum();
+
+    //Getter for getting row sum result
+    public int getRowSum() {
+        return rowSum;
+    }
+
+    @Override
+    public void run() {
+        rowSum = Arrays.stream(numberGroups[rowIndex]).sum();
+    }
 }
--- src/ait/numbers/model/ThreadGroupSum.java
+++ src/ait/numbers/model/ThreadGroupSum.java
@@ -1,13 +1,45 @@
 package ait.numbers.model;
 
+import java.util.Arrays;
+
 public class ThreadGroupSum extends GroupSum {
     public ThreadGroupSum(int[][] numberGroups) {
         super(numberGroups);
     }
 
+    public ThreadGroupSum(int[][] numberGroups, int rowIndex) {
+        super(numberGroups, rowIndex);
+    }
+
     @Override
     public int computeSum() {
         // TODO ThreadGroupSum
-        return 0;
+        int rows = numberGroups.length;
+
+        GroupSum[] tasks = new GroupSum[rows];
+
+        // Create tasks
+        for (int i = 0; i < rows; i++) {
+            tasks[i] = new ThreadGroupSum(numberGroups, i);
+        }
+
+        Thread[] threads = new Thread[rows];
+        
+        //Create and start our threads
+        for (int i = 0; i < rows; i++) {
+            threads[i] = new Thread(tasks[i]);
+            threads[i].start();
+        }
+
+        // Waiting for all threads
+        for (int i = 0; i < rows; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return Arrays.stream(tasks).mapToInt(GroupSum::getRowSum).sum();
     }
 }
