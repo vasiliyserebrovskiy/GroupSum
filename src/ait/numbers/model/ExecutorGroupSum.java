f03ae6ab132305f9ae2af0f62a609c33f86cb0fe
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-13 19:42:09 +0200

    realization of ExecutorGroupSum added

--- src/ait/numbers/model/ExecutorGroupSum.java
+++ src/ait/numbers/model/ExecutorGroupSum.java
@@ -1,5 +1,10 @@
 package ait.numbers.model;
 
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorGroupSum extends GroupSum{
     public ExecutorGroupSum(int[][] numberGroups) {
         super(numberGroups);
@@ -8,6 +13,31 @@ public class ExecutorGroupSum extends GroupSum{
     @Override
     public int computeSum() {
         // TODO ExecutorGroupSum
-        return 0;
+        int rows = numberGroups.length;
+
+        GroupSum[] tasks = new GroupSum[rows];
+
+        // Create tasks
+        for (int i = 0; i < rows; i++) {
+            tasks[i] = new ThreadGroupSum(numberGroups, i);
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < rows; i++) {
+            executorService.execute(tasks[i]);
+        }
+
+        executorService.shutdown();
+
+        //waiting for calculation
+        try {
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return Arrays.stream(tasks).mapToInt(GroupSum::getRowSum).sum();
     }
 }
